7e225efdf71ad57c3165613dc88d4bcbaab71798
ramonis567 <ramonmatosferreira@gmail.com>
2021-01-29 17:39:19 -0300

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,34 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import db from '../db.json';
+
+const theme = db.theme;
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+* {
+  box-sizing: border-box;
 }
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  font-family: 'Lato', Arial, Helvetica, sans-serif;
+  color: ${({ theme }) => theme.colors.contrastText};
+}
+html, body {
+  min-height: 100vh;
+}
+#__next {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
