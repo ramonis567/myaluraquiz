99c354327d0c7801127d3f6ccdec991b4a3f6f15
ramonis567 <ramonmatosferreira@gmail.com>
2021-02-11 00:48:01 -0300

    query param on finished component

---
+++ pages/quiz/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function QuizDaGaleraPage() {
+  return (
+    <div style={{ color: 'black', }}>
+      Próxima aula
+    </div>
+  )
+}
--- src/components/Finished/index.js
+++ src/components/Finished/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Widget from '../Widget';
+import { useRouter } from 'next/router';
 
 export default function Finished({ results, totalQuestions, correctCounter }) {
+  const { query } = useRouter();
+
   return (
     <Widget>
       <Widget.Header>
         <h1>Resultados:</h1>
       </Widget.Header>
       <Widget.Content>
+        <h3>Jogador(a): {query.nickname}</h3>
+        <hr />
         {results.map((result, index) => {
           const textResult =  `Questão ${index + 1}: `;
           if(result){
@@ -16,8 +21,8 @@ export default function Finished({ results, totalQuestions, correctCounter }) {
             return <p>{textResult}INCORRETO</p>
           }
         })}
-        <h4>Acertos: {correctCounter}/{totalQuestions}</h4>
-
+        <hr />
+        <h2>Desempenho: {correctCounter}/{totalQuestions} acertos</h2>        
       </Widget.Content>
     </Widget>
   )
